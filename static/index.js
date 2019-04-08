e13799e7b52bdcbc21e02113569463103cbe3de1
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2019-04-08 23:34:38 +0200

    trying to create socket

--- static/index.js
+++ static/index.js
@@ -2,11 +2,18 @@
   document.addEventListener("DomContentLoaded", () => {
       var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
 
+      alert("qui!");
       socket.on("connect", () => {
           document.querySelectorAll("button").forEach(button => {
               button.onclick = () => {
+                  alert("lancio emit");
                   socket.emit("evento");
               };
           });
       });
-  }
+
+      socket.on("registrato", () => {
+        document.querySelector("body").innerHTML += "registrato!";
+      });
+  });
+
